e4ea630c94289d3e69714e6a0ec7a379e8eb3e13
codewithharshit-commit <sharma.harshit.0008@gmail.com>
2025-10-24 00:30:46 +0530

    Solved PrintAlternate java dsa problem

---
+++ ArraysProblems/PrintAlternates.java
@@ -0,0 +1,19 @@
+package ArraysProblems;
+
+import java.util.*;
+
+class PrintAlternates {
+
+    static ArrayList<Integer> getAlternates(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i += 2) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println(getAlternates(arr));
+    }
+}
