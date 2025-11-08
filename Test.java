fbfb24a9e817ba77795a1b11404519793b70ace2
Harshit <harshit@Harshits-MacBook-Air.local>
2025-11-08 17:52:36 +0530

    start to linked list

--- Test.java
+++ Test.java
@@ -21,8 +21,29 @@ public class Test {
         System.out.println(secondMax);
     }
     public static void main(String[] args) {
-        int[] arr= {1,2,3,2,0,0,1};
-        // 
-        secondLargest(arr);
+        int[] arr= {1,2,3,2,0,0,1}; 
+        //secondLargest(arr);
+        int[] newArr = {1,0,1,0,0,1,1,0};
+        SortZeroAndOneArray.sortZeroAndOneArray(newArr);
+    }
+}
+
+class SortZeroAndOneArray{
+    static void sortZeroAndOneArray(int[] arr){
+        int len = arr.length;
+        int zeroCount = 0;
+        for(int i=0;i<len;i++){
+            if(arr[i]==0) zeroCount++;
+        }
+        int[] newArray = new int[len];
+        for(int j=0;j<zeroCount-1;j++){
+            arr[j] = 0;
+        }
+        for(int j=zeroCount;j<len;j++){
+            arr[j] = 1;
+        }
+        for(int i = 0;i<len;i++){
+            System.out.print(arr[i]);
+        }
     }
 }
